c97c6a2fbd7771e1f94268d393c8c7157bf3dfc7
Akshita-Saini <60578602+Akshita-Saini@users.noreply.github.com>
2022-02-27 02:11:50 +0530

    restructured the public folder and refactored the styles in home.module.css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,51 +1,47 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useState } from "react";
+import clearUIPreview from "../public/images/clearUI.png";
+import noteshubPreview from "../public/images/noteshub.png";
+import ezHabitPreview from "../public/images/ezHabit.png";
 
 export default function Home() {
-  const [projectHoverState, setProjectHoverState] = useState({
-    clearUI:false,
-    noteshub:false,
-    ezHabit:false
-  });
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Akshita Saini</title>
         <meta name="description" content="Akshita Saini's Portfolio" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <div className={styles.nav}>
-        <span className={styles.logo}>
-          <Image src="/brand-icon.svg" alt="Brand Logo" width={60} height={60} />
-        </span>
+        <Image src="/icons/brand-icon.svg" alt="Brand Logo" width={60} height={60} />
         <span className={styles.brandname}>
           Akshita Saini
         </span>
       </div>
       
-      <main className={styles.main}>
+      <main>
         <header className={styles.header}>
           <div className={styles.headerText}>
             <div>Hi,</div>
             <div>I&#39;m <span className={styles.highlight}>Akshita Saini</span>.</div>
             <div>I&#39;m a <span className={styles.darkHighlight}>developer</span>!</div>
           </div>
-          <div className={styles.headerImage}>
-            <Image src="/profile.jpg" alt="Akshita's Profile Pic" width={500} height={500}/>
+          <div>
+            <Image src="/images/profile.jpg" alt="Akshita's Profile Pic" priority="true" width={500} height={500}/>
           </div>
         </header>
 
         <section className={styles.sectionOne}>
-          <h2 className={styles.sectionHeading}>Check out my projects here <span><Image className={styles.pointer} src="/pointer.svg" alt="pointer down" width={64} height={64}/></span></h2>
+          <h2 className={styles.sectionOneHeading}>Check out my projects here <span><Image className={styles.pointer} src="/icons/pointer.svg" alt="pointer down" width={64} height={64}/></span></h2>
 
           <div className={styles.card}>
-            <Image className={styles.cardImage} height={455} width={935} alt="project preview" src="/clearUI.png"/>
+            <Image className={styles.cardImage} height={455} width={935} placeholder="blur" quality={100} alt="project preview" src={clearUIPreview}/>
             <div className={styles.cardBody} >
               <h4 className={styles.cardHeading}>Clear Ui 
-              <a href="https://clear-ui.netlify.app/"><Image className={styles.arrowTopRight} alt="arrow top right" src="/arrow-top-right.svg" height={72} width={72} /></a>
+              <a href="https://clear-ui.netlify.app/"><Image  alt="arrow top right" src="/icons/arrow-top-right.svg" height={72} width={72} /></a>
               </h4>
               <div className={styles.cardText}>A CSS component library with components that can be composed to build any design, directly in your markup. It&#39;s easy to customize and use.</div>
               <div className={styles.btnGroup}>
@@ -56,12 +52,10 @@ export default function Home() {
           </div>
 
           <div className={styles.cardReverse}>
-            <div className={styles.imageContainer}>
-              <Image className={styles.cardImage} width={935} height={455} alt="project preview" src="/noteshub.png"/>
-            </div>
+            <Image className={styles.cardImage} width={935} height={455} placeholder="blur" quality={100} alt="project preview" src={noteshubPreview}/>
             <div className={styles.cardBody} >
               <h4 className={styles.cardHeading}>Noteshub
-                <a href="https://noteshub.netlify.app/"><Image className={styles.arrowTopRight} alt="arrow top right" src="/arrow-top-right.svg" height={72} width={72} /></a>
+                <a href="https://noteshub.netlify.app/"><Image alt="arrow top right" src="/icons/arrow-top-right.svg" height={72} width={72} /></a>
               </h4>
               <div className={styles.cardText}>A front-end only note-taking app made using React. Supports create, edit and delete note. The user may also tag notes.</div>
               <div className={styles.btnGroup}>
@@ -72,12 +66,10 @@ export default function Home() {
           </div>
 
           <div className={styles.card}>
-            <div className={styles.imageContainer}>
-              <Image className={styles.cardImage} width={935} height={455} alt="project preview" src="/ezHabit.png"/>
-            </div>
+            <Image className={styles.cardImage} width={935} height={455} placeholder="blur" quality={100} alt="project preview" src={ezHabitPreview} />
             <div className={styles.cardBody} >
               <h4 className={styles.cardHeading}>ezHabit
-              <a href="https://ezhabit.netlify.app/"><Image className={styles.arrowTopRight} alt="arrow top right" src="/arrow-top-right.svg" height={72} width={72} /></a>
+              <a href="https://ezhabit.netlify.app/"><Image alt="arrow top right" src="/icons/arrow-top-right.svg" height={72} width={72} /></a>
               </h4>
               <div className={styles.cardText}>A gamified Habit tracking app created using ReactJS, TypeScript, Node.js, Express and MongoDB. Work on this is ongoing!</div>
               <div className={styles.btnGroup}>
@@ -90,17 +82,17 @@ export default function Home() {
         </section>
 
         <section className={styles.sectionTwo}>
-          <h2 className={styles.sectionHeading}>Connect with me <span><Image className={styles.pointer} src="/pointer.svg" alt="pointer down" width={25} height={25}/></span></h2>
+          <h2 className={styles.sectionTwoHeading}>Connect with me</h2>
           <div className={styles.socialIcons}>
-            <a href="https://www.instagram.com/code_with_akshita/"><Image alt="instagram" src="/instagram.png" height={64} width={64} /></a>
-            <a href="https://twitter.com/AkshitaSaini15"><Image alt="twitter" src="/twitter.png" height={64} width={64} /></a>
-            <a href="https://www.linkedin.com/in/akshita-saini15/"><Image alt="linkedin" src="/linkedin.png" height={64} width={64} /></a>
-            <a href="https://github.com/Akshita-Saini/"><Image alt="github" src="/github.png" height={64} width={64} /></a>
+            <a href="https://www.instagram.com/code_with_akshita/"><Image alt="instagram" src="/images/instagram.png" height={64} width={64} /></a>
+            <a href="https://twitter.com/AkshitaSaini15"><Image alt="twitter" src="/images/twitter.png" height={64} width={64} /></a>
+            <a href="https://www.linkedin.com/in/akshita-saini15/"><Image alt="linkedin" src="/images/linkedin.png" height={64} width={64} /></a>
+            <a href="https://github.com/Akshita-Saini/"><Image alt="github" src="/images/github.png" height={64} width={64} /></a>
           </div>
         </section>
 
         <footer className={styles.footer}>
-          Made with love <Image className='heart' src="/heart.svg" alt="heart" height={18} width={18}/> by Akshita 
+          Made with love <Image className='heart' src="/icons/heart.svg" alt="heart" height={18} width={18}/> by Akshita 
         </footer>
       </main>
     </div>
