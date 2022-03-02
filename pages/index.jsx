3962355b9f5cff0f8706a06e8ade1a0386edeb60
Akshita-Saini <60578602+Akshita-Saini@users.noreply.github.com>
2022-03-03 02:10:45 +0530

    fixed a tag error

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import { Card } from "../src/components/Card";
 import { Navigation } from "../src/components/Navigation";
 import { Footer } from "../src/components/Footer";
@@ -13,48 +13,69 @@ export default function Home() {
     <div>
       <Head>
         <title>Akshita Saini</title>
-        <meta name="description" content="Akshita Saini's Portfolio Home Page" />
+        <meta
+          name="description"
+          content="Akshita Saini's Portfolio Home Page"
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <Navigation />
-      
+      <Navigation />
+
       <main>
         <header className={styles.header}>
           <div className={styles.headerText}>
             <div>Hi,</div>
-            <div>{`I'm `}<span className={styles.highlight}>Akshita Saini</span>.</div>
-            <div>{`I'm a `} <span className={styles.darkHighlight}>developer</span>!</div>
+            <div>
+              {`I'm `}
+              <span className={styles.highlight}>Akshita Saini</span>.
+            </div>
+            <div>
+              {`I'm a `} <span className={styles.darkHighlight}>developer</span>
+              !
+            </div>
           </div>
           <div className={styles.headerImageContainer}>
-            <Image src="/images/profile.jpg" alt="Akshita's Profile Pic" priority="true" width={500} height={500}/>
+            <Image
+              src="/images/profile.jpg"
+              alt="Akshita's Profile Pic"
+              priority="true"
+              width={500}
+              height={500}
+            />
           </div>
         </header>
 
         <section className={styles.sectionOne}>
           <h2 className={styles.sectionOneHeading}>
-            <Image  src="/icons/projects.svg" alt="projects" width={64} height={64}/><span>projects.</span>
+            <Image
+              src="/icons/projects.svg"
+              alt="projects"
+              width={64}
+              height={64}
+            />
+            <span>projects.</span>
           </h2>
-          <Card 
-            imageURL={clearUIPreview} 
-            projectName="Clear UI"  
+          <Card
+            imageURL={clearUIPreview}
+            projectName="Clear UI"
             projectURL="https://clear-ui.netlify.app/"
             githubURL="https://github.com/Akshita-Saini/Clear-UI"
             projectDesc={`A CSS component library with components that can be composed to build any design, directly in your markup. It's easy to customize and use.`}
             cardClass={styles.card}
           />
-          <Card 
-            imageURL={noteshubPreview} 
-            projectName="Noteshub"  
+          <Card
+            imageURL={noteshubPreview}
+            projectName="Noteshub"
             projectURL="https://noteshub.netlify.app/"
             githubURL="https://github.com/Akshita-Saini/noteshub"
             projectDesc={`A front-end only note-taking app made using React. Supports create, edit and delete note. The user may also tag notes.`}
             cardClass={styles.cardReverse}
           />
-          <Card 
-            imageURL={ezHabitPreview} 
-            projectName="ezHabit"  
+          <Card
+            imageURL={ezHabitPreview}
+            projectName="ezHabit"
             projectURL="https://ezhabit.netlify.app/"
             githubURL="https://github.com/Akshita-Saini/ezhabit"
             projectDesc={`A gamified Habit tracking app created using ReactJS, TypeScript, Node.js, Express and MongoDB. Work on this is ongoing!`}
@@ -64,18 +85,72 @@ export default function Home() {
 
         <section className={styles.sectionTwo}>
           <h2 className={styles.sectionTwoHeading}>
-            <Image  src="/icons/connect.svg" alt="connect" width={64} height={64}/><span>  {`let's connect.`}</span>
+            <Image
+              src="/icons/connect.svg"
+              alt="connect"
+              width={64}
+              height={64}
+            />
+            <span> {`let's connect.`}</span>
           </h2>
           <div className={styles.socialIcons}>
-            <a href="https://www.instagram.com/code_with_akshita/"><Image alt="instagram" src="/images/instagram.png" height={64} width={64} /> Instagram </a>
-            <a href="https://twitter.com/AkshitaSaini15"><Image alt="twitter" src="/images/twitter.png" height={64} width={64} /> Twitter </a>
-            <a href="https://www.linkedin.com/in/akshita-saini15/"><Image alt="linkedin" src="/images/linkedin.png" height={64} width={64} /> LinkedIn</a>
-            <a href="https://github.com/Akshita-Saini/"><Image alt="github" src="/images/github.png" height={64} width={64} /> Github </a>
+            <a
+              href="https://www.instagram.com/code_with_akshita/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <Image
+                alt="instagram"
+                src="/images/instagram.png"
+                height={64}
+                width={64}
+              />{" "}
+              Instagram{" "}
+            </a>
+            <a
+              href="https://twitter.com/AkshitaSaini15"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <Image
+                alt="twitter"
+                src="/images/twitter.png"
+                height={64}
+                width={64}
+              />{" "}
+              Twitter{" "}
+            </a>
+            <a
+              href="https://www.linkedin.com/in/akshita-saini15/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <Image
+                alt="linkedin"
+                src="/images/linkedin.png"
+                height={64}
+                width={64}
+              />{" "}
+              LinkedIn
+            </a>
+            <a
+              href="https://github.com/Akshita-Saini/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <Image
+                alt="github"
+                src="/images/github.png"
+                height={64}
+                width={64}
+              />{" "}
+              Github{" "}
+            </a>
           </div>
         </section>
 
         <Footer />
       </main>
     </div>
-  )
+  );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,51 @@
-import Image from 'next/image'
+import Image from "next/image";
 import styles from "../../styles/Card.module.css";
 
-const Card = ({imageURL, projectName, projectURL, projectDesc, githubURL, cardClass}) => (
-    <div className={cardClass}>  
-        <div className={styles.cardImageContainer}>
-            <Image className={styles.cardImage} height={300} width={500} placeholder="blur" quality={100} alt="project preview" src={imageURL}/>
-            <div className={styles.cardImageDesign}>
-            </div>
-        </div>
-        <div className={styles.cardBody} >
-            <div className={styles.cardHeading}>{projectName}</div>
-            <div className={styles.cardText}>
-                {projectDesc}
-            </div>
-            <div className={styles.btnGroup}>
-                <a href={githubURL}><Image src="/icons/github.svg" className={styles.githubBtnIcon} height={36} width={36} alt="Github Icon" /></a>  
-                <a href={projectURL} className={styles.btn}>View Project</a>
-            </div>
-        </div>
-   </div>
+const Card = ({
+  imageURL,
+  projectName,
+  projectURL,
+  projectDesc,
+  githubURL,
+  cardClass,
+}) => (
+  <div className={cardClass}>
+    <div className={styles.cardImageContainer}>
+      <Image
+        className={styles.cardImage}
+        height={300}
+        width={500}
+        placeholder="blur"
+        quality={100}
+        alt="project preview"
+        src={imageURL}
+      />
+      <div className={styles.cardImageDesign}></div>
+    </div>
+    <div className={styles.cardBody}>
+      <div className={styles.cardHeading}>{projectName}</div>
+      <div className={styles.cardText}>{projectDesc}</div>
+      <div className={styles.btnGroup}>
+        <a href={githubURL} target="_blank" rel="noreferrer noopener">
+          <Image
+            src="/icons/github.svg"
+            className={styles.githubBtnIcon}
+            height={36}
+            width={36}
+            alt="Github Icon"
+          />
+        </a>
+        <a
+          href={projectURL}
+          className={styles.btn}
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          View Project
+        </a>
+      </div>
+    </div>
+  </div>
 );
 
-export {Card};
+export { Card };
